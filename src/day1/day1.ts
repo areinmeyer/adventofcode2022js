c05bbeb23b9c36cc215bd9babf553203f6187cc7
Allen Reinmeyer <areinmeyer@gmail.com>
2022-12-06 09:40:37 -0500

    add linting and re-format

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,63 +1,62 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import * as R from 'ramda'
+import * as fs from "fs";
+import * as path from "path";
+import * as R from "ramda";
 
 const getElves = (list: string[]): number[][] => {
-    let elfList : number[][] = []
-    let elfFood :number[] = []
-    for (let i = 0; i < list.length; i++) {
-        if (list[i] == '') {
-            elfList.push(elfFood);
-            elfFood = []
-        }
-        else {
-            const num = parseInt(list[i])
-            elfFood.push(num)
-        }
-    }
-    return elfList;
-}
+	const elfList : number[][] = [];
+	let elfFood :number[] = [];
+	for (let i = 0; i < list.length; i++) {
+		if (list[i] == "") {
+			elfList.push(elfFood);
+			elfFood = [];
+		}
+		else {
+			const num = parseInt(list[i]);
+			elfFood.push(num);
+		}
+	}
+	return elfList;
+};
 
 const getElfTotalCalories = (elf: number[]) => {
-    return elf.reduce((totalCals, calories) => {
-        return totalCals + calories
-    })
-}
+	return elf.reduce((totalCals, calories) => {
+		return totalCals + calories;
+	});
+};
 
 const foldElves = (elves: number[][]): number[] => {
-    return elves.map((elf) => {
-        return getElfTotalCalories(elf)
-    })
-}
+	return elves.map((elf) => {
+		return getElfTotalCalories(elf);
+	});
+};
 
 const determineMostCalories = (elfTotals: number[]): number => {
-    const maxCalories = 0;
-    return elfTotals.reduce((maxCalories, calorie) => {
-        return maxCalories > calorie ? maxCalories : calorie
-    })
-}
+	return elfTotals.reduce((maxCalories, calorie) => {
+		return maxCalories > calorie ? maxCalories : calorie;
+	});
+};
 
 const determineTopNMostCalories = (elfTotals: number[], n: number): number[] => {
-    const sorter = (a: number,b: number) => { return a - b}
-    return R.reverse(R.sort(sorter,elfTotals)).slice(0,3)
-}
+	const sorter = (a: number,b: number) => { return a - b;};
+	return R.reverse(R.sort(sorter,elfTotals)).slice(0,n);
+};
 
 const part1 = (calorieString: string) => {
-    const allcalories = calorieString.split('\n')
-    return determineMostCalories(foldElves(getElves(allcalories)))
-}
+	const allcalories = calorieString.split("\n");
+	return determineMostCalories(foldElves(getElves(allcalories)));
+};
 
 const part2 = (calorieString: string) => {
-    const allcalories = calorieString.split('\n')
-    const maxCalories = determineTopNMostCalories(foldElves(getElves(allcalories)),3)
-    let totals: number = maxCalories.reduce((totals, calorie) => {
-        totals = totals + calorie
-        return totals
-    })
-    return totals
-}
+	const allcalories = calorieString.split("\n");
+	const maxCalories = determineTopNMostCalories(foldElves(getElves(allcalories)),3);
+	const totals: number = maxCalories.reduce((totals, calorie) => {
+		totals = totals + calorie;
+		return totals;
+	});
+	return totals;
+};
 
-const list = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8')
-console.log(`Part 1 result: ${part1(list)}`)
-console.log(`Part 2 result: ${part2(list)}`)
-export { getElves }
+const list = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+console.log(`Part 1 result: ${part1(list)}`);
+console.log(`Part 2 result: ${part2(list)}`);
+export { getElves };
--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -1,121 +1,121 @@
-import * as fs from 'fs'
-import * as path from 'path'
+import * as fs from "fs";
+import * as path from "path";
 // import * as R from 'ramda'
 
 const player1Options: {[key: string]: string} = {
-    "A": "Rock",
-    "B": "Paper",
-    "C": "Scissors"
-}
+	"A": "Rock",
+	"B": "Paper",
+	"C": "Scissors"
+};
 const player2Options: {[key: string]: string} = {
-    "X": "Rock",
-    "Y": "Paper",
-    "Z": "Scissors"
-}
+	"X": "Rock",
+	"Y": "Paper",
+	"Z": "Scissors"
+};
 
 const choicePoints: {[key: string]: number} = {
-    "Rock": 1,
-    "Paper": 2,
-    "Scissors": 3
-}
+	"Rock": 1,
+	"Paper": 2,
+	"Scissors": 3
+};
 
 const part2Strategies: {[key: string]: string} = {
-    "X": "lose",
-    "Y": "draw",
-    "Z": "win"
-}
+	"X": "lose",
+	"Y": "draw",
+	"Z": "win"
+};
 
 const scoreTheRound = (player1: string, player2: string): number => {
-    const player1Choice = player1Options[player1]
-    const player2Choice = player2Options[player2]
+	const player1Choice = player1Options[player1];
+	const player2Choice = player2Options[player2];
 
-    let score = 0
-    if (player1Choice === player2Choice) {
-        score = 3 + choicePoints[player1Choice]
-    }
-    else if(player1Choice === 'Rock') {
-        if (player2Choice === 'Scissors')
-            score = 0 + choicePoints[player2Choice]
-        else
-            score = 6 + choicePoints[player2Choice]
-    }
-    else if (player1Choice === 'Scissors') {
-        if (player2Choice === 'Paper')
-            score = 0 + choicePoints[player2Choice]
-        else
-            score = 6 + choicePoints[player2Choice]
-    }
-    else if (player1Choice === 'Paper') {
-        if (player2Choice === 'Rock')
-            score = 0 + choicePoints[player2Choice]
-        else
-            score = 6 + choicePoints[player2Choice]
-    }
-    return score
-}
+	let score = 0;
+	if (player1Choice === player2Choice) {
+		score = 3 + choicePoints[player1Choice];
+	}
+	else if(player1Choice === "Rock") {
+		if (player2Choice === "Scissors")
+			score = 0 + choicePoints[player2Choice];
+		else
+			score = 6 + choicePoints[player2Choice];
+	}
+	else if (player1Choice === "Scissors") {
+		if (player2Choice === "Paper")
+			score = 0 + choicePoints[player2Choice];
+		else
+			score = 6 + choicePoints[player2Choice];
+	}
+	else if (player1Choice === "Paper") {
+		if (player2Choice === "Rock")
+			score = 0 + choicePoints[player2Choice];
+		else
+			score = 6 + choicePoints[player2Choice];
+	}
+	return score;
+};
 
 const getMyPick = (player1: string, strategy: string): string => {
-    if (strategy === 'win') {
-        switch (player1) {
-            case "Rock":
-                return 'Paper'
-            case 'Scissors':
-                return 'Rock'
-            case 'Paper':
-                return 'Scissors'
-        }
-    }
-    else if (strategy === 'lose') {
-        switch (player1) {
-            case "Rock":
-               return 'Scissors'
-            case "Scissors":
-                return "Paper"
-            case "Paper":
-                return "Rock"
-        }
-    }
-    return player1
-}
+	if (strategy === "win") {
+		switch (player1) {
+		case "Rock":
+			return "Paper";
+		case "Scissors":
+			return "Rock";
+		case "Paper":
+			return "Scissors";
+		}
+	}
+	else if (strategy === "lose") {
+		switch (player1) {
+		case "Rock":
+			return "Scissors";
+		case "Scissors":
+			return "Paper";
+		case "Paper":
+			return "Rock";
+		}
+	}
+	return player1;
+};
 const determineYourPick = (player1: string, result: string): number => {
 
-    const roundResult = part2Strategies[result]
-    const player1Choice = player1Options[player1]
-    const myPick = getMyPick(player1Choice, roundResult)
-    if (roundResult === 'draw')
-        return 3 + choicePoints[myPick]
-    if (roundResult === 'win')
-        return 6 + choicePoints[myPick]
-    if (roundResult === 'lose')
-        return 0 + choicePoints[myPick]
-    return 0
-}
+	const roundResult = part2Strategies[result];
+	const player1Choice = player1Options[player1];
+	const myPick = getMyPick(player1Choice, roundResult);
+	if (roundResult === "draw")
+		return 3 + choicePoints[myPick];
+	if (roundResult === "win")
+		return 6 + choicePoints[myPick];
+	if (roundResult === "lose")
+		return 0 + choicePoints[myPick];
+	return 0;
+};
 
 const part1 = (input: string) => {
-    const list = input.split('\n')
-    const scores: number[] = list.map((round) => {
-        const choices = round.split(' ')
-        const [player1, player2] = choices
-        return scoreTheRound(player1, player2)
-    })
-    let total = scores.reduce((total: number, score: number) => {
-        return total + score
-    })
-    return total;
-}
+	const list = input.split("\n");
+	const scores: number[] = list.map((round) => {
+		const choices = round.split(" ");
+		const [player1, player2] = choices;
+		return scoreTheRound(player1, player2);
+	});
+	const total = scores.reduce((total: number, score: number) => {
+		return total + score;
+	});
+	return total;
+};
 
 const part2 = (input: string) => {
-    const list = input.split('\n')
-    const scores: number[] = list.map((round) => {
-        const choices = round.split(' ')
-        const [player1, player2] = choices
-        return determineYourPick(player1, player2)
-    })
-    let total = scores.reduce((total: number, score: number) => {
-        return total + score
-    })
-    return total
-}
-const list = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8')
-console.log("Part 1 answer: ", part1(list))
-console.log("Part 2 answer: ", part2(list))
+	const list = input.split("\n");
+	const scores: number[] = list.map((round) => {
+		const choices = round.split(" ");
+		const [player1, player2] = choices;
+		return determineYourPick(player1, player2);
+	});
+	const total = scores.reduce((total: number, score: number) => {
+		return total + score;
+	});
+	return total;
+};
+const list = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+console.log("Part 1 answer: ", part1(list));
+console.log("Part 2 answer: ", part2(list));
